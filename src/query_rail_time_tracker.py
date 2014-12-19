2dcfb456b4c8fd825560477f5a2f5a33cc38327f
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2014-12-19 08:14:35 -0600

    reading train schedule and parsing into python native formats

--- src/query_rail_time_tracker.py
+++ src/query_rail_time_tracker.py
@@ -4,7 +4,7 @@
 (https://metrarail.com/metra/wap/en/home/RailTimeTracker.html) to
 determine what the current status is on a given route in the Metra
 system. You should start running this script ~5 minutes before the
-train is due to arrive in the station. 
+train is due to arrive in the station.
 
 pass a trip_id to this script, which then looks up the schedule for
 this particular trip and monitors its timeliness.
@@ -18,30 +18,30 @@ import json
 
 import requests
 
-from utils import strip
+from utils import strip, js2pydate
 
+# trip_id = sys.argv[1]
+# gtfs_dir = sys.argv[1]
 
-gtfs_dir = sys.argv[1]
-
-# TODO: this can be moved into the actual scraper. the scraper can
-# read in the schedule when it first launches based on the trip_id
-
-# read in the stop times to find all of the trips. this information
-# needs to be passed along to the
-trips = collections.defaultdict(list)
-with open(os.path.join(gtfs_dir, 'stop_times.txt')) as stream:
-    reader = csv.DictReader(stream)
-    for row in map(strip, reader):
-        trips[row['trip_id']].append((
-            row['stop_sequence'],
-            row['stop_id'],
-            row['departure_time'],
-        ))
-
-# have this script end prematurely 
-print >> sys.stderr, "NOT DONE YET"
-sys.exit(0)
-fuck
+# # TODO: this can be moved into the actual scraper. the scraper can
+# # read in the schedule when it first launches based on the trip_id
+#
+# # read in the stop times to find all of the trips. this information
+# # needs to be passed along to the
+# trips = collections.defaultdict(list)
+# with open(os.path.join(gtfs_dir, 'stop_times.txt')) as stream:
+#     reader = csv.DictReader(stream)
+#     for row in map(strip, reader):
+#         trips[row['trip_id']].append((
+#             row['stop_sequence'],
+#             row['stop_id'],
+#             row['departure_time'],
+#         ))
+#
+# # have this script end prematurely
+# print >> sys.stderr, "NOT DONE YET"
+# sys.exit(0)
+# fuck
 
 stations = (
     "ELBURN",
@@ -65,28 +65,23 @@ stations = (
     "OTC",
 )
 
-
-data = {"stationRequest": { 
+# post a request to the API
+post_data = {"stationRequest": {
     "Corridor": "UP-W",
     "Destination": "OTC",
-    "Origin": "GENEVA",
-#    "timestamp": "/Date(1402364517333-0000)/"
+    "Origin": stations[2],
 }}
 headers = {'content-type': 'application/json'}
-
-
 response = requests.post(
     "http://12.205.200.243/AJAXTrainTracker.svc/GetAcquityTrainData",
     # "http://75.144.111.115/AJAXTrainTracker.svc/GetAcquityTrainData", #backup?
-    data=json.dumps(data),
+    data=json.dumps(post_data),
     headers=headers,
 )
+
+# extract the estimated departure time for this post.
 if response.status_code == 200:
     result = json.loads(response.text)
-    import pprint
-    pprint.pprint(json.loads(result['d']))
-
-# print dir(response)
-# print response.status_code
-# print response.text
-
+    data = json.loads(result['d'])
+    train1 = data['train1']
+    print train1['train_num'], js2pydate(train1['estimated_dpt_time']), js2pydate(train1['scheduled_dpt_time']),
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,20 @@
+import re
+import datetime
+
+
 def strip(row):
+    """strip all extra space off of native python objects"""
     if isinstance(row, dict):
         return dict((k.strip(), v.strip()) for k, v in row.iteritems())
     elif isinstance(row, list):
         return [x.strip() for x in row]
     else:
         raise TypeError
+
+def js2pydate(js_string):
+    """Convert the gross '/Date(#########)/' format to a datetime"""
+    timestamp = re.match(
+        '/Date\((?P<timestamp>[0-9]+)\)/',
+        js_string,
+    ).groupdict()['timestamp']
+    return datetime.datetime.fromtimestamp(int(timestamp)/1000)
